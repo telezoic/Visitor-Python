7d2d58f27d8c223418376e29287833332beb1c7a
telezoic <daniel.sifton@viu.ca>
2015-09-21 14:25:10 -0700

    initial commit

---
+++ visitor.py
@@ -0,0 +1,48 @@
+#utf-8
+#python 2.7.10
+#Daniel Sifton
+
+import csv
+import time
+from webdriverplus import WebDriver
+from itertools import *
+
+csvfile = raw_input("Enter the name of the csv input file . . .")
+loopcall = raw_input("Infinite loop? (yes/no) . . .")
+delay = raw_input("Time delay for each URL visit (in seconds) . . . ")
+delay = int(delay)
+
+urls = csv.reader(open(csvfile, "rU"))
+
+if loopcall == "yes":
+
+	for url in cycle(urls):
+
+		browser = WebDriver('firefox', reuse_browser=True)
+
+		browser.maximize_window()
+
+		browser.get(url[0])
+
+		time.sleep(delay)
+
+elif loopcall == "no":
+
+	for url in urls:
+
+		browser = WebDriver('firefox', reuse_browser=True)
+
+		browser.maximize_window()
+
+		browser.get(url[0])
+
+		time.sleep(delay)
+
+		browser.quit()
+
+else:
+
+	print "Please enter yes or no to specify the loop . . . "
+
+
+		
